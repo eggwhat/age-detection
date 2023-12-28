7ffeadc9745ae0405e4d9af0a29aa29034592aad
eggwhat <piotr.padamczyk@gmail.com>
2023-12-28 20:28:36 +0100

    Add endpoint for handling multiple images

--- backend/api.py
+++ backend/api.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse, JSONResponse
 from core.face_detection import detect_faces
 from core.video_processing import generate_video, process_video
-from pydantic import BaseModel
-from typing import List
 from PIL import Image
+from typing import List
 import shutil
 import tempfile
+import zipfile
 import os
 import cv2
 import numpy as np
@@ -58,12 +58,36 @@ async def detect_age_single(file: UploadFile = File(...)):
         return JSONResponse(content={"detail": f"An error occurred: {str(e)}"}, status_code=500)
 
 
-# @app.post("/detect-age/multiple")
-# async def detect_age_multiple(file: UploadFile = File(...)):
+@app.post("/detect-age/multiple")
+def detect_age_multiple(files: List[UploadFile] = File(...)):
+    if not all(file.content_type.startswith("image/") for file in files):
+        raise HTTPException(status_code=400, detail="Invalid file format. Please upload an image.")
+    try:
+        images = []
+        for file in files:
+            content = file.file.read()
+            image_array = cv2.imdecode(np.frombuffer(content, np.uint8), -1)
 
+            detected_faces = detect_faces(image_array)
+            if not detected_faces['faces'].any():
+                raise HTTPException(status_code=400, detail="No faces detected in the image.")
 
-class Frame(BaseModel):
-    content: bytes
+            image_pil = Image.fromarray(cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB))
+            output_image = io.BytesIO()
+            image_pil.save(output_image, format="PNG")
+            output_image.seek(0)
+            images.append({"name": file.filename, "content": output_image.getvalue()})
+
+        zip_file = io.BytesIO()
+        with zipfile.ZipFile(zip_file, 'w') as zipf:
+            for image in images:
+                zipf.writestr(image["name"], image["content"])
+
+        zip_file.seek(0)
+        return StreamingResponse(iter([zip_file.getvalue()]), media_type="application/x-zip-compressed",
+                                 headers={"Content-Disposition": f"attachment; filename=images.zip"})
+    except Exception as e:
+        return JSONResponse(content={"detail": f"An error occurred: {str(e)}"}, status_code=500)
 
 
 @app.post("/detect-age/video")
@@ -85,6 +109,7 @@ async def detect_age_video(file: UploadFile = File(...)):
         return StreamingResponse(io.BytesIO(video_bytes), media_type="video/mp4")
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
 
 if __name__ == "__main__":
     import uvicorn
