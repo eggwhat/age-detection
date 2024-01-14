f4a83900b06a3cb8dece2d970faa6a0f37d83d7a
grioool <olga.a.grigorieva@gmail.com>
2024-01-14 21:23:59 +0100

    new buttons position

--- frontend/age-detection/src/App.js
+++ frontend/age-detection/src/App.js
@@ -59,41 +59,46 @@ function App() {
 
     const handleImageUpload = async (files) => {
 
-    if (files.length > 40) {
-        alert('Please select a directory with fewer than 40 files.');
-        return;
-    }
-
-    const formData = new FormData();
-    files.forEach(file => {
-        formData.append('files', file);
-    });
-
-    try {
-        const response = await fetch('http://127.0.0.1:8000/detect-age/multiple', {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (!response.ok) {
-            throw new Error('Server responded with an error.');
+        if (files.length === 0) {
+            alert('Please select a directory with one or more files.');
+            return;
         }
 
-        const blob = await response.blob();
-        const downloadUrl = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = downloadUrl;
-        link.download = 'images.zip';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        window.URL.revokeObjectURL(downloadUrl);
+        if (files.length > 40) {
+            alert('Please select a directory with fewer than 40 files.');
+            return;
+        }
 
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Upload failed. Please try again.');
-    }
-};
+        const formData = new FormData();
+        files.forEach(file => {
+            formData.append('files', file);
+        });
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/detect-age/multiple', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (!response.ok) {
+                throw new Error('Server responded with an error.');
+            }
+
+            const blob = await response.blob();
+            const downloadUrl = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.download = 'images.zip';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(downloadUrl);
+
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Upload failed. Please try again.');
+        }
+    };
 
 
     const handleVideoUpload = async (file) => {
@@ -139,8 +144,37 @@ function App() {
                 <p>Our advanced AI will guess your age from your webcam feed. Give it a try!</p>
             </header>
             <main>
+                <div className="button-container">
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleFileSelect}
+                        multiple
+                        directory=""
+                        webkitdirectory=""
+                        hidden
+                        id="imageUploadDirectory"
+                    />
+                    <label htmlFor="imageUploadDirectory" className="upload-button">Upload Image Directory</label>
+
+                    <input
+                        type="file"
+                        accept="video/*"
+                        onChange={(e) => handleVideoUpload(e.target.files[0])}
+                        hidden
+                        id="videoUpload"
+                    />
+                    <label htmlFor="videoUpload" className="upload-button">Upload Video</label>
+                </div>
+
+                {processedImageData && (
+                    <img src={`data:image/jpeg;base64,${processedImageData}`} alt="Processed Image"/>
+                )}
+                {processedVideoData && (
+                    <video controls src={processedVideoData}/>
+                )}
+
                 <div className="video-container">
-                    {/*{!isPredictionReceived ? (*/}
                     <div className="webcam-feed">
                         <Webcam
                             audio={false}
@@ -149,33 +183,10 @@ function App() {
                             videoConstraints={{width: 640, height: 480}}
                         />
                     </div>
-                    {/*) : (*/}
                     <div className="result-display">
                         {processedFrame && <img src={processedFrame} alt="Processed Frame"/>}
                     </div>
-                    {/*)}*/}
                 </div>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileSelect}
-                    multiple
-                    directory=""
-                    webkitdirectory=""
-                    hidden
-                    id="imageUploadDirectory"
-                />
-                <label htmlFor="imageUploadDirectory" className="upload-button">Upload Image Directory</label>
-
-                <input type="file" accept="video/*" onChange={(e) => handleVideoUpload(e.target.files[0])} hidden
-                       id="videoUpload"/>
-                <label htmlFor="videoUpload" className="upload-button">Upload Video</label>
-                {processedImageData && (
-                    <img src={`data:image/jpeg;base64,${processedImageData}`} alt="Processed Image"/>
-                )}
-                {processedVideoData && (
-                    <video controls src={processedVideoData}/>
-                )}
             </main>
             <footer>
                 <p>&copy; 2024 Age Detection App</p>
